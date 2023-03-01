c71875282e6705e96bfd539c43b08a4df8be9515
Dennis Goc-ong <86005147+m-dennisgocong@users.noreply.github.com>
2023-03-02 04:56:28 +0700

    import setTimeLength Component and used it twice
    used setTimeLength Component to create settings for timer length (session and break).

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import './App.css'
-
+import SetTimeLength from './components/setTimeLength';
 function App() {
   const [session, setSession] = useState<number>(25);
   const [breakTime, setBreakTime] = useState<number>(5);
@@ -8,6 +8,16 @@ function App() {
   return (
     <div className="App">
       <h1 className='main-title'>Pomodoer</h1>
+        <SetTimeLength
+          name={"break"} 
+          time={breakTime} 
+          handleTime={setBreakTime}
+        />
+        <SetTimeLength
+          name={"session"} 
+          time={session} 
+          handleTime={setSession}
+        />
     </div>
   )
 }
