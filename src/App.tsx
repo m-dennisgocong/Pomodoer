0f33ac7f68bc2e96d230a0b6e9544e4932b45c1b
Dennis Goc-ong <86005147+m-dennisgocong@users.noreply.github.com>
2023-03-02 04:50:35 +0700

    feat: add h1 for title "Pomodoer"

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,12 @@ import { useState } from 'react';
 import './App.css'
 
 function App() {
-
   const [session, setSession] = useState<number>(25);
   const [breakTime, setBreakTime] = useState<number>(5);
 
   return (
     <div className="App">
-
+      <h1 className='main-title'>Pomodoer</h1>
     </div>
   )
 }
