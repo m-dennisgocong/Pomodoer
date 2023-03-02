d78ccbbf16a5bf5f760b582763e7ca71e2ed4925
Dennis Goc-ong <86005147+m-dennisgocong@users.noreply.github.com>
2023-03-03 05:44:41 +0700

    remove handleDecrement & handleIncrement function
    It's remove since the main app have one function "handleTimerLength"  that can handle both functionality of the two function

--- src/components/setTimeLength.tsx
+++ src/components/setTimeLength.tsx
@@ -1,27 +1,21 @@
 export interface Props {
     name : string;
     time : number;
-    handleTime : Function;
+    handleTimerLength : Function;
 }
 
-function setTimeLength({time, handleTime, name} : Props){
+function setTimeLength({time, handleTimerLength, name} : Props){
 
-    function handleDecrement(){
-        return time <= 1 ? handleTime(1) : handleTime(time - 1); 
-    }
-    function handleIncrement(){
-        return time >= 60 ? handleTime(60) : handleTime(time + 1);
-    }   
     function nameLabel(){  
         return name.charAt(0).toUpperCase() + name.slice(1) + " Length";
     }
-    
+       
     return (
         <div className={`length-setting`}>
             <div id={`${name}-label`}>{`${nameLabel()}`}</div>
-            <button id={`${name}-decrement`} onClick={() => handleDecrement()}></button>
+            <button id={`${name}-decrement`} onClick={() => handleTimerLength(name, time - 1)}></button>
             <div id={`${name}-length`}>{time}</div>
-            <button id={`${name}-increment`} onClick={() => handleIncrement()}></button> 
+            <button id={`${name}-increment`} onClick={() => handleTimerLength(name, time + 1)}></button> 
         </div>
     );
 }
