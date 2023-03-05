6ef8ef79dec498dac59e66c1413d1fcb1fb20fd8
Dennis Goc-ong <86005147+m-dennisgocong@users.noreply.github.com>
2023-03-06 05:27:21 +0700

    feat: add two react-icons arrow (up&down) and separate function for decrement and increment

--- src/components/setTimeLength.tsx
+++ src/components/setTimeLength.tsx
@@ -1,3 +1,4 @@
+import { BsArrowDown, BsArrowUp } from "react-icons/bs";
 export interface Props {
     name : string;
     time : number;
@@ -6,16 +7,28 @@ export interface Props {
 
 function setTimeLength({time, handleTimerLength, name} : Props){
 
-    function nameLabel(){  
+    const nameLabel = () : string => {  
         return name.charAt(0).toUpperCase() + name.slice(1) + " Length";
     }
-       
+
+    const increment = () : number => {
+        return handleTimerLength(name, time > 1 ? time - 1 : time);
+    }
+    
+    const decrement = () : number => {
+        return handleTimerLength(name, time < 60 ? time + 1 : time);
+    }
+    
     return (
         <div className={`length-setting`}>
             <div id={`${name}-label`}>{`${nameLabel()}`}</div>
-            <button id={`${name}-decrement`} onClick={() => handleTimerLength(name, time - 1)}></button>
+            <button id={`${name}-decrement`} onClick={() => increment()}>
+                <BsArrowDown />
+            </button>
             <div id={`${name}-length`}>{time}</div>
-            <button id={`${name}-increment`} onClick={() => handleTimerLength(name, time + 1)}></button> 
+            <button id={`${name}-increment`} onClick={() => decrement()}>
+                <BsArrowUp />
+            </button> 
         </div>
     );
 }
