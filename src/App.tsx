81eb6c4350245a9da7c9fb837fa7992602351377
Dennis Goc-ong <86005147+m-dennisgocong@users.noreply.github.com>
2023-03-09 05:24:45 +0700

    Add audio functionality for countdown timer
    This commit adds the necessary audio functionality for the countdown timer, as specified in user stories #28, #29, and #30. The "beep" audio element will now stop and rewind to the beginning when the "reset" button is clicked (#28), and the audio file is now required to be at least one second long (#29). Additionally, when the countdown reaches zero, the "beep" sound will play (#30) to indicate that time is up. This functionality was implemented using HTML5 audio tags with corresponding id attributes.
    
    Fixes #28 #29 #30

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import useInterval from 'use-interval';
 import './App.css'
 import SetTimeLength from './components/setTimeLength';
 import Timer from './components/Timer';
+import Beep from './assets/BeepSound.wav';
 
 export interface TimerState {
   [key : string] : number | string | boolean ;
@@ -11,6 +12,7 @@ export interface TimerState {
   timerState: boolean;
   currentTimer: string;
   timeLeft: number;
+
 }
 
 function App() {
@@ -20,7 +22,7 @@ function App() {
     break: 5,
     timerState: false,
     currentTimer: "Session",
-    timeLeft: 1500
+    timeLeft: 1500,
   };
 
   const [timer, setTimer] = useState<TimerState>(initialState);
@@ -35,11 +37,22 @@ function App() {
       ...updateTimer, timerState: options}));
   }
   const handleReset = () : void => {
-    return setTimer(updateTimer => ({
+    setTimer(updateTimer => ({
       ...updateTimer, ...initialState
     }));
+    alarm(false);
   }
-  
+
+  const alarm = (setAlarm : boolean) => {
+    const audio = document.getElementById("beep") as HTMLAudioElement;
+    if(setAlarm){
+      audio.play();
+    }else{
+      audio.pause();
+      audio.currentTime = 0;
+    }
+  }
+
   const switchTimer = (str : string, num : number) : void =>  {
     return setTimer(updateTimer => ({
       ...updateTimer, currentTimer : str, timeLeft: num}))
@@ -50,6 +63,7 @@ function App() {
       setTimer(updateTimer => ({
       ...updateTimer, timeLeft: (updateTimer.timeLeft - 1)})) 
     }else{
+      alarm(true);
       timer.currentTimer === 'Session' ?  
       switchTimer('Break', (timer.break * 60)) : switchTimer('Session', timer.session * 60);
     }
@@ -75,6 +89,7 @@ function App() {
         play_pause={handlePlay_Pause}
         handleReset={handleReset}
         />
+      <audio id="beep" preload="auto" src={Beep}></audio>
     </div>
   );
 }
